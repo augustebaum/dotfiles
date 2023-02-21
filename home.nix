1618cec24f3941463ee6809bf9d3678fc95c3847
Auguste Baum <auguste.apple@gmail.com>
2023-02-21 22:25:42 +0100

    dura auto-backup

--- home.nix
+++ home.nix
@@ -186,7 +186,8 @@
     QMK_HOME = "${CONFIG}/qmk/qmk_firmware";
 
     ## starship
-    STARSHIP_CONFIG = "${CONFIG}/starship/config.toml";
+    # STARSHIP_CONFIG = "${CONFIG}/starship/config.toml";
+    STARSHIP_CONFIG = builtins.toString ./config/starship/config.toml;
   };
 
   home.shellAliases = with config.home.sessionVariables; {
